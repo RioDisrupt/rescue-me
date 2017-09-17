aa0fb988c34a19eb008d1cf15929e3db1077a08f
Lauren <lauren.parkos@gmail.com>
2017-09-17 07:40:00 -0700

    Adding styling to templates and changing copy

--- app/components/PickScreen.jsx
+++ app/components/PickScreen.jsx
@@ -6,18 +6,14 @@ export default class PickScreen extends React.Component{
     return(
       <div>
         <div className="picker-top">
-          <div className="gethelp">
             <a href="">
               Get Help
             </a>
-          </div>
         </div>
         <div className="picker-bottom">
-          <div className="givehelp">
             <a href="">
               Offer Help
             </a>
-          </div>
         </div>
       </div>
     )
